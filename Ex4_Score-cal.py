0c80b6c793a7609424eb05d53110d74248178e65
AloeThron <98453438+AloeThron@users.noreply.github.com>
2022-01-27 12:19:41 +0700

    go

---
+++ Ex4_Score-cal.py
@@ -0,0 +1,10 @@
+FE = 81.4
+GB = 92.8
+IC = 88.3
+CP = 86.7
+
+print("--- You Score ---")
+print("Foundation English :", FE)
+print("General Business :", GB)
+print("Introduction to Computer Systems :", IC)
+print("Computer Programming :", CP)
